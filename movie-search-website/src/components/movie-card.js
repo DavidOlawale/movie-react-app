17e40945e96d19cb6bc077ac294ff30097548d0a
queen-favour <elechifavour3@gmail.com>
2023-09-15 08:55:01 +0100

    changes to movie card

--- movie-search-website/src/components/movie-card.js
+++ movie-search-website/src/components/movie-card.js
@@ -6,6 +6,7 @@ const MovieCard = ({ poster_path, title, release_date, id }) => {
     navigate(`/movies/${id}`, { replace: false });
   };
 
+
   return (
     <div className="cursor-pointer" onClick={clicked}>
         <img
@@ -18,16 +19,16 @@ const MovieCard = ({ poster_path, title, release_date, id }) => {
         </p>
     
       <div className="flex flex-col">
-        <p className="text-gray-500 text-min text-left py-1 block">
-          USA, 2016 - current
+        <p className="text-gray-500 text-min text-left py-1 block" data-testid="movie-release-date">
+          {release_date}
         </p>
-
         <span className="text-lg font-semibold text-black block">{title}</span>
 
         <span className="flex flex-row justify-between w-full my-1">
           <p className="block text-xs text-gray-500">IMDB 80D/100</p>
-          <p className="block text-xs text-gray-500">##97%</p>
+          <p className="block text-xs text-gray-500">#97%</p>
         </span>
+  
 
         <span className="text-gray-500 text-min">
           Action, Adventure, Horror
